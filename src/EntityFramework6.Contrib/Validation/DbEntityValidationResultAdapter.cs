7147cc6ceb71f6bf99c5d228c72ec6b96432eeb7
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-24 16:20:35 +0100

    Add IDbEntityValidationResult adapter implementation

--- src/EntityFramework6.Contrib/Validation/DbEntityValidationResultAdapter.cs
+++ src/EntityFramework6.Contrib/Validation/DbEntityValidationResultAdapter.cs
@@ -1,17 +1,29 @@
+using EntityFramework6.Contrib.Infrastructure;
 using System;
+using System.Collections.Generic;
 using System.Data.Entity.Validation;
+using System.Linq;
 
 namespace EntityFramework6.Contrib.Validation
 {
-    // TODO: implement this
     internal class DbEntityValidationResultAdapter : IDbEntityValidationResult
     {
         private readonly DbEntityValidationResult _adaptee;
+        private readonly Lazy<IDbEntityEntry> _entry;
+        private readonly Lazy<ICollection<IDbValidationError>> _validationErrors;
 
         public DbEntityValidationResultAdapter(DbEntityValidationResult adaptee)
         {
             if (adaptee == null) throw new ArgumentNullException(nameof(adaptee));
             _adaptee = adaptee;
+            _entry = new Lazy<IDbEntityEntry>(() => new DbEntityEntryAdapter(_adaptee.Entry));
+            _validationErrors = new Lazy<ICollection<IDbValidationError>>(
+                () => _adaptee.ValidationErrors.Select(validationError => new DbValidationErrorAdapter(validationError)).ToArray()
+            );
         }
+
+        public IDbEntityEntry Entry => _entry.Value;
+        public bool IsValid => _adaptee.IsValid;
+        public ICollection<IDbValidationError> ValidationErrors => _validationErrors.Value;
     }
 }
