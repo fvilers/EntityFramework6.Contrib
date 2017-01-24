d62b1e22e4c70bb9d19bbae057b50e7c2090b486
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-24 16:13:48 +0100

    Add IDbValidationError adapter

---
+++ src/EntityFramework6.Contrib/Validation/DbValidationErrorAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity.Validation;
+
+namespace EntityFramework6.Contrib.Validation
+{
+    internal class DbValidationErrorAdapter : IDbValidationError
+    {
+        private readonly DbValidationError _adaptee;
+
+        public DbValidationErrorAdapter(DbValidationError adaptee)
+        {
+            if (adaptee == null) throw new ArgumentNullException(nameof(adaptee));
+            _adaptee = adaptee;
+        }
+
+        public string ErrorMessage => _adaptee.ErrorMessage;
+        public string PropertyName => _adaptee.PropertyName;
+    }
+}
