28a4b2abbbcf1c187dccdd4e3dffa47d0067d196
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-24 15:39:34 +0100

    Add IDbContextConfiguration members

--- src/EntityFramework6.Contrib/Infrastructure/IDbContextConfiguration.cs
+++ src/EntityFramework6.Contrib/Infrastructure/IDbContextConfiguration.cs
@@ -2,5 +2,11 @@ namespace EntityFramework6.Contrib.Infrastructure
 {
     public interface IDbContextConfiguration
     {
+        bool AutoDetectChangesEnabled { get; set; }
+        bool EnsureTransactionsForFunctionsAndCommands { get; set; }
+        bool LazyLoadingEnabled { get; set; }
+        bool ProxyCreationEnabled { get; set; }
+        bool UseDatabaseNullSemantics { get; set; }
+        bool ValidateOnSaveEnabled { get; set; }
     }
 }
