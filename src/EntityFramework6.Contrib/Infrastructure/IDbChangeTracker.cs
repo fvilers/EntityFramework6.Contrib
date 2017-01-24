cc8760cd4ce7b22408deb878469c4f2078cebe74
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-24 15:35:07 +0100

    Add IDbChangeTracker members

--- src/EntityFramework6.Contrib/Infrastructure/IDbChangeTracker.cs
+++ src/EntityFramework6.Contrib/Infrastructure/IDbChangeTracker.cs
@@ -1,6 +1,12 @@
-﻿namespace EntityFramework6.Contrib.Infrastructure
+﻿using System.Collections.Generic;
+
+namespace EntityFramework6.Contrib.Infrastructure
 {
     public interface IDbChangeTracker
     {
+        void DetectChanges();
+        IEnumerable<IDbEntityEntry> Entries();
+        IEnumerable<IDbEntityEntry<TEntity>> Entries<TEntity>() where TEntity : class;
+        bool HasChanges();
     }
 }
