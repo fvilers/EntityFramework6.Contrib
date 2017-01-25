fbe59d20cc5e615ec10f8a986af41d66337dbcfa
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-25 10:22:04 +0100

    Add IDbReferenceEntry adapter implementation

--- src/EntityFramework6.Contrib/Infrastructure/DbReferenceEntryAdapter.cs
+++ src/EntityFramework6.Contrib/Infrastructure/DbReferenceEntryAdapter.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace EntityFramework6.Contrib.Infrastructure
 {
-    // TODO: implement this
     internal class DbReferenceEntryAdapter : DbMemberEntryAdapter, IDbReferenceEntry
     {
         private readonly DbReferenceEntry _adaptee;
@@ -13,6 +15,37 @@ namespace EntityFramework6.Contrib.Infrastructure
         {
             if (adaptee == null) throw new ArgumentNullException(nameof(adaptee));
             _adaptee = adaptee;
+        }
+
+        public bool IsLoaded
+        {
+            get { return _adaptee.IsLoaded; }
+            set { _adaptee.IsLoaded = value; }
+        }
+
+        public IDbReferenceEntry<TEntity, TProperty> Cast<TEntity, TProperty>() where TEntity : class
+        {
+            return new DbReferenceEntryAdapter<TEntity, TProperty>(_adaptee.Cast<TEntity, TProperty>());
+        }
+
+        public void Load()
+        {
+            _adaptee.Load();
+        }
+
+        public Task LoadAsync()
+        {
+            return _adaptee.LoadAsync();
+        }
+
+        public Task LoadAsync(CancellationToken cancellationToken)
+        {
+            return _adaptee.LoadAsync(cancellationToken);
+        }
+
+        public IQueryable Query()
+        {
+            return _adaptee.Query();
         }
     }
 }
--- src/EntityFramework6.Contrib/Infrastructure/IDbReferenceEntry.cs
+++ src/EntityFramework6.Contrib/Infrastructure/IDbReferenceEntry.cs
@@ -1,6 +1,17 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace EntityFramework6.Contrib.Infrastructure
 {
     public interface IDbReferenceEntry : IDbMemberEntry
     {
+        bool IsLoaded { get; set; }
+
+        IDbReferenceEntry<TEntity, TProperty> Cast<TEntity, TProperty>() where TEntity : class;
+        void Load();
+        Task LoadAsync();
+        Task LoadAsync(CancellationToken cancellationToken);
+        IQueryable Query();
     }
 }
