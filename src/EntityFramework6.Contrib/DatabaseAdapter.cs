a1a34e41377b5a2a03d0a310ac7728941e188427
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-25 10:33:32 +0100

    Add IDatabase adapter

---
+++ src/EntityFramework6.Contrib/DatabaseAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace EntityFramework6.Contrib
+{
+    // TODO: implement this
+    public class DatabaseAdapter : IDatabase
+    {
+        private readonly Database _adaptee;
+
+        public DatabaseAdapter(Database adaptee)
+        {
+            if (adaptee == null) throw new ArgumentNullException(nameof(adaptee));
+            _adaptee = adaptee;
+        }
+    }
+}
