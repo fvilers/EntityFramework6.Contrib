133f0588a5fdd08895100085cfbee4b09daedeec
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-25 10:18:10 +0100

    Add IDbPropertyEntry adapter implementation

--- src/EntityFramework6.Contrib/Infrastructure/DbPropertyEntryAdapter.cs
+++ src/EntityFramework6.Contrib/Infrastructure/DbPropertyEntryAdapter.cs
@@ -3,16 +3,36 @@ using System.Data.Entity.Infrastructure;
 
 namespace EntityFramework6.Contrib.Infrastructure
 {
-    // TODO: implement this
     internal class DbPropertyEntryAdapter : DbMemberEntryAdapter, IDbPropertyEntry
     {
         private readonly DbPropertyEntry _adaptee;
+        private readonly Lazy<IDbComplexPropertyEntry> _parentProperty;
 
         public DbPropertyEntryAdapter(DbPropertyEntry adaptee)
             : base(adaptee)
         {
             if (adaptee == null) throw new ArgumentNullException(nameof(adaptee));
             _adaptee = adaptee;
+            _parentProperty = new Lazy<IDbComplexPropertyEntry>(() => new DbComplexPropertyEntryAdapter(adaptee.ParentProperty));
+        }
+
+        public bool IsModified
+        {
+            get { return _adaptee.IsModified; }
+            set { _adaptee.IsModified = value; }
+        }
+
+        public object OriginalValue
+        {
+            get { return _adaptee.OriginalValue; }
+            set { _adaptee.OriginalValue = value; }
+        }
+
+        public IDbComplexPropertyEntry ParentProperty => _parentProperty.Value;
+
+        public IDbPropertyEntry<TEntity, TProperty> Cast<TEntity, TProperty>() where TEntity : class
+        {
+            return new DbPropertyEntryAdapter<TEntity, TProperty>(_adaptee.Cast<TEntity, TProperty>());
         }
     }
 }
--- src/EntityFramework6.Contrib/Infrastructure/IDbPropertyEntry.cs
+++ src/EntityFramework6.Contrib/Infrastructure/IDbPropertyEntry.cs
@@ -2,5 +2,10 @@ namespace EntityFramework6.Contrib.Infrastructure
 {
     public interface IDbPropertyEntry : IDbMemberEntry
     {
+        bool IsModified { get; set; }
+        object OriginalValue { get; set; }
+        IDbComplexPropertyEntry ParentProperty { get; }
+
+        IDbPropertyEntry<TEntity, TProperty> Cast<TEntity, TProperty>() where TEntity : class;
     }
 }
